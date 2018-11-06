1e93d6bd5a7ce0a2ea4080e0c0149c8e6c057095
takumimas <takumi.mas@hotmail.com>
2018-11-06 21:07:43 +0900

    add file

---
+++ recaptcha.py
@@ -0,0 +1,14 @@
+import json
+import urllib.request
+import urllib.parse
+def recaptcha(key,token):
+    url = "https://www.google.com/recaptcha/api/siteverify"
+    post_data = {
+        "secret":" {Secret key} ",
+        "response" : str(token)
+    }
+    post_data = urllib.parse.urlencode(post_data).encode("utf-8")
+    with urllib.request.urlopen(url, data=post_data) as res:
+        ans = res.read().decode("utf-8")
+        ans = json.loads(ans)
+    return ans["success"]
